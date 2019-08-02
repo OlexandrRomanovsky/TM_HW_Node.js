c6517547e3612f5661ee703cbbee6995356a3a48
Olexandr <olexa.romanowsky@gmail.com>
2019-08-02 07:01:44 +0300

    try to add Autorization

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,15 +1,16 @@
 const Joi = require("joi");
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-let todoList = require('./data');
-let uniqid = require('uniqid');
+let todoList = require("./data");
+let uniqid = require("uniqid");
+var jwt = require("jsonwebtoken");
 
 const todo = {
   id: uniqid(),
   text: "",
   done: false
-}
+};
 
 app.use(bodyParser.json());
 
@@ -19,20 +20,22 @@ app.delete("/todos/:id", removeTodo);
 app.patch("/todos/:id/done", markDone);
 app.patch("/todos/:id/undone", markUndone);
 
-function getTodos(req, res) {
+;function getTodos(req, res) {
   if (todoList.length === 0) return res.status(404).send("Data base is void");
   res.json(todoList);
 }
 
 function addTodo(req, res) {
   const schema = {
-    text: Joi.string().min(1).required()
+    text: Joi.string()
+      .min(1)
+      .required()
   };
   const result = Joi.validate(req.body, schema);
-  console.log('result =>', result);
-  (result.error) && res.status(400).send(result.error.details[0].message); 
+  console.log("result =>", result);
+  result.error && res.status(400).send(result.error.details[0].message);
 
-  const newTodo = Object.assign({}, todo, req.body) ;
+  const newTodo = Object.assign({}, todo, req.body);
   res.json(newTodo);
   todoList.push(newTodo);
   res.send(`Updated task`);
@@ -41,11 +44,13 @@ function addTodo(req, res) {
 function removeTodo(req, res) {
   const id = req.params.id;
   const schema = {
-    id: Joi.string().min(1).required()
+    id: Joi.string()
+      .min(1)
+      .required()
   };
   const result = Joi.validate(req.params, schema);
-  (result.error) && res.status(400).send(result.error.details[0].message);
-  let bool = todoList.findIndex(todo => +todo.id === +id)
+  result.error && res.status(400).send(result.error.details[0].message);
+  let bool = todoList.findIndex(todo => +todo.id === +id);
   if (bool === -1) return res.status(400).send("Try another id");
   todoList = todoList.filter(todo => todo.id !== id);
   res.send(`Removed task id: ${id}`);
@@ -56,10 +61,9 @@ function markDone(req, res) {
   todoList.forEach(todo => {
     if (todo.id === id) {
       todo.done = true;
-    } 
-    
+    }
   });
-  let markExists = todoList.filter(todo => todo.id === id)
+  let markExists = todoList.filter(todo => todo.id === id);
   if (markExists.length === 0) return res.status(400).send("Try another id");
   res.send(`Done task id: ${id}`);
 }
@@ -71,12 +75,52 @@ function markUndone(req, res) {
       todo.done = false;
     }
   });
-  let markExists = todoList.filter(todo => todo.id === id)
+  let markExists = todoList.filter(todo => todo.id === id);
   if (markExists.length === 0) return res.status(400).send("Try another id");
   res.send(`Undone task id: ${id}`);
 }
 
-const port = process.env.Port || 2000
+app.post('/api/posts', verifyToken, (req, res) => {  
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 10,
+    name: "Tom",
+    email: "tom@mail.com"
+  }
+
+  jwt.sign({user}, "sekretkey", (err, token) => {
+    res.json({
+      token
+    })
+  }) 
+})
+
+function verifyToken (req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  if(typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403)
+  }
+}
+
+
+const port = process.env.Port || 2000;
 
 app.listen(port, function() {
   console.log("Listen port", port);
